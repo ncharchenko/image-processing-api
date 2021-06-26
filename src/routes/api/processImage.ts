99023f58fec63480fa79ceaa180190cb0c476f58
Nick Charchenko <ncharchenko@Nicks-MacBook-Air.local>
2021-06-26 08:02:33 -0700

    Removing res.send from processImage, calling endpoint causes some infinite loop.

--- src/routes/api/processImage.ts
+++ src/routes/api/processImage.ts
@@ -9,45 +9,13 @@ const path = require('path');
 const dir = path.resolve('../../../cache');
 const sharp = require(`sharp`);
 
-let validateQuery = function(req: express.Request, res: express.Response) {
-    let fileName;
-    let height;
-    let width;
-    if (req.query.fileName) {
-        fileName = req.query.filename;
-    }
-    else {
-        res.status(400).send(`Missing filename`);
-    }
-    if (req.query.height) {
-        height = req.query.height;
-    }
-    else {
-        res.status(400).send(`Missing height`);
-    }
-    if (req.query.width) {
-        width = req.query.width;
-    }
-    else {
-        res.status(400).send(`Missing width`);
-    }
-
-    // If our query is valid, build and return a JSON object
-    let query = {
-        fileName: fileName,
-        height: height,
-        width: width
-    };
-
-    return JSON.stringify(query);
-}
-
 let resizeImage = async function(fileName: string, height: number, width: number) {
     // Resize image
     try {
         const filePath: string = path.join("../../../images",fileName)
         const resizedFileName = `${fileName}-${height}-${width}.jpg`
         sharp(filePath).resize(height,width).jpeg({quality: 50}).toFile(fs.join(dir,resizedFileName));
+        console.log("Resizing image");
         return fs.readFile(fs.join(dir,resizedFileName));
     } catch {
         // Catch an error with image resizing
@@ -71,9 +39,39 @@ let getImage = async function(query: string) {
     }
 }
 
-processImage.get('/', logger, (req, res) => {
-    let query: string = validateQuery(req, res); // Validate request query
-    res.send(getImage(query));
+processImage.get('/', logger, (req) => {
+    console.log("processImage route");
+    let fileName;
+    let height;
+    let width;
+    if (req.query.fileName) {
+        fileName = req.query.filename;
+    }
+    else {
+        return `Missing filename`;
+    }
+    if (req.query.height) {
+        height = req.query.height;
+    }
+    else {
+        return `Missing height`;
+    }
+    if (req.query.width) {
+        width = req.query.width;
+    }
+    else {
+        return `Missing width`;
+    }
+
+    // If our query is valid, build a JSON string to pass into getImage.
+    let query = {
+        fileName: fileName,
+        height: height,
+        width: width
+    };
+
+    let args: string = JSON.stringify(query);
+    return getImage(args);
 });
 
 export default processImage;
