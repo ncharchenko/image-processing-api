10256d333440f6047b085e7d2dafcbc1bec8603b
Nick Charchenko <ncharchenko@Nicks-MacBook-Air.local>
2021-07-09 11:25:46 -0500

    Latest

--- src/routes/api/processImage.ts
+++ src/routes/api/processImage.ts
@@ -6,7 +6,7 @@ const processImage = express.Router();
 
 const fs = require('fs');
 const path = require('path');
-const dir = path.resolve('cache');
+const dir: string = path.resolve('cache');
 const sharp = require(`sharp`);
 
 const resizeImage = function(fileName: string, height: number, width: number) {
@@ -14,14 +14,13 @@ const resizeImage = function(fileName: string, height: number, width: number) {
     try {
         const img_dir = path.resolve('images');
         const filePath: string = path.join(img_dir,fileName);
-        const img = fs.accessSync(filePath);
         let fileNameSplit = fileName.split('.');
         const resizedFileName = `${fileNameSplit[0]}-${height}-${width}.jpg`;
         if (!fs.existsSync(dir)){
             fs.mkdirSync(dir);
         }
         sharp(filePath).resize(height,width).jpeg({quality: 50}).toFile(path.join(dir,resizedFileName));
-        return path.join(dir,resizedFileName);
+        return path.join(dir,resizedFileName) as string;
     } catch (err) {
         // Catch an error with image resizing
         console.log(err);
