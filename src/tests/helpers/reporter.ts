ce210da13e637ac8671e0f5691c71c65a08f83f6
Nick Charchenko <ncharchenko@Nicks-MacBook-Air.local>
2021-09-02 11:16:47 -0700

    New reporter.ts file.

--- src/tests/helpers/reporter.ts
+++ src/tests/helpers/reporter.ts
@@ -1,18 +1,20 @@
-import { DisplayProcessor, SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
-import SuiteInfo = jasmine.SuiteInfo;
-
-class CustomProcessor extends DisplayProcessor {
+import {
+    DisplayProcessor,
+    SpecReporter,
+    StacktraceOption,
+  } from 'jasmine-spec-reporter';
+  import SuiteInfo = jasmine.JasmineStartedInfo;
+  import CustomReporter = jasmine.CustomReporter;
+  class CustomProcessor extends DisplayProcessor {
     public displayJasmineStarted(info: SuiteInfo, log: string): string {
-        return `${log}`;
+      return `${log}`;
     }
-}
-
-jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(
+  }
+  jasmine.getEnv().addReporter(
     new SpecReporter({
-        spec: {
-            displayStacktrace: StacktraceOption.NONE,
-        },
-        customProcessors: [CustomProcessor],
-    }),
-);
+      spec: {
+        displayStacktrace: StacktraceOption.NONE,
+      },
+      customProcessors: [CustomProcessor],
+    }) as CustomReporter
+  );
